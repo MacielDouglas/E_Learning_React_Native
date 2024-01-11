b25f61e06b7a5312359e9323e25e0ecd4f63b073
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-11 11:33:23 -0300

    Criado area de login com ClerkProvider

--- e-learning/App.js
+++ e-learning/App.js
@@ -2,21 +2,25 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './App/Screen/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    outfit: require('./assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    outfit_regular: require('./assets/fonts/Outfit-Regular.ttf'),
+    outfit_medium: require('./assets/fonts/Outfit-Medium.ttf'),
+    outfit_bold: require('./assets/fonts/Outfit-Bold.ttf'),
   });
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-      {/* <Text style={{ fontFamily: 'outfit-bold' }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" /> */}
-    </View>
+    <ClerkProvider publishableKey="mafufu">
+      <View style={styles.container}>
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
--- e-learning/App/Screen/LoginScreen.js
+++ e-learning/App/Screen/LoginScreen.js
@@ -1,10 +1,34 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import Colors from '../Utils/Colors';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useWarmUpBrowser } from '../../hooks/warmUpBrowser.tsx';
 import app from './../../assets/images/145054.png';
 import google from '../../assets/images/google-color-svgrepo-com.png';
 
+WebBrowser.maybeCompleteAuthSession();
+
 export default function LoginScreen() {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  }, []);
+
   return (
     <View style={{ display: 'flex', alignItems: 'center' }}>
       <Image
@@ -25,7 +49,7 @@ export default function LoginScreen() {
             textAlign: 'center',
             fontSize: 35,
             color: Colors.WHITE,
-            fontFamily: 'outfit-bold',
+            fontFamily: 'outfit_bold',
             marginTop: 20,
           }}
         >
@@ -37,12 +61,13 @@ export default function LoginScreen() {
             fontSize: 20,
             marginTop: 20,
             color: Colors.LIGHT_PRIMARY,
-            fontFamily: 'outfit',
+            fontFamily: 'outfit_regular',
           }}
         >
           O Melhor Local Para Aprender Programação
         </Text>
         <TouchableOpacity
+          onPress={onPress}
           style={{
             backgroundColor: Colors.WHITE,
             display: 'flex',
